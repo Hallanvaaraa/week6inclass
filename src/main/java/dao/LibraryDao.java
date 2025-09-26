bba2e646784d7b1eddf7c04a0335ca98ca17dbb6
Tommi <tommi.s.halla@gmail.com>
2025-09-26 10:26:18 +0300

    Student CRUD added to DAO

--- src/main/java/dao/LibraryDao.java
+++ src/main/java/dao/LibraryDao.java
@@ -1,11 +1,75 @@
 package dao;
 
+import entity.Student;
 import jakarta.persistence.*;
 
 public class LibraryDao {
 
     private EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("week6inClass");
+
+    public void saveStudent(entity.Student student) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(student);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Student findStudentById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Student.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void updateStudent(Student student) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.merge(student);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteStudent(Long id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Student student = em.find(Student.class, id);
+            if (student != null) {
+                em.remove(student);
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
 
 
 
